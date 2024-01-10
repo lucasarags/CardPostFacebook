33139a8a3c2263d382e788b116e3c8292961847f
Lucas Aragao Alves da Costa <lucas.arags@gmail.com>
2024-01-10 10:22:55 -0300

    Projeto Card Post Facebook Exercicio

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,51 @@
+import iredeIcon from "../../assets/irede.png";
+import restic12 from "../../assets/restic12.png";
+import { IoMdGlobe } from "react-icons/io";
+import { BiLike } from "react-icons/bi";
+import { MdOutlineChat } from "react-icons/md";
+import { PiArrowElbowUpRight } from "react-icons/pi";
+function Header() {
+  return (
+    <div className="flex items-center ">
+      <img src={iredeIcon} alt="Irede Icon" className=" m-4" />
+      <div>
+        <h2 className="font-bold text-xs text-black">Instituto IREDE</h2>
+        <h3 className="text-xs flex items-center text-gray-400 font-medium gap-x-0.5">
+          12h
+          <IoMdGlobe />
+        </h3>
+      </div>
+    </div>
+  );
+}
+function Body() {
+  return (
+    <div>
+      <img src={restic12} alt="Inscrição Restic12" className="w-72 h-48"></img>
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <div className="flex items-center items-center p-3 text-xs text-gray-400 font-medium space-x-9 ">
+      <section className="flex items-center gap-1">
+        Curtir <BiLike />
+      </section>
+      <section className="flex items-center gap-1">
+        Comentar <MdOutlineChat />
+      </section>
+      <section className="flex items-center gap-1 ">
+        Comentar <PiArrowElbowUpRight />
+      </section>
+    </div>
+  );
+}
+export default function Card() {
+  return (
+    <div className="w-72 mx-4 my-7 shadow-md shadow-gray-400">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header class="container h-16 bg-blue-500 ">
+      <h1 class="p-5 leading-6 text-white text-xl font-bold">Facebook</h1>
+    </header>
+  );
+}
